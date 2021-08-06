155508f03baf8e3cee19725d567657501352931e
xing.wu <>
2021-08-06 18:38:44 +0800

    添加分数获取

--- src/score.js
+++ src/score.js
@@ -35,6 +35,49 @@ const getScore = async (cookies) => {
 
     console.log(today,dayScoreDtos);
 
+    let rule_list = [{
+        ruleId: 1,
+        name: 'article_num', //阅读文章
+    }, {
+        ruleId: 2,
+        name: 'video_num', //视听学习
+    }, {
+        ruleId: 9,
+        name: 'login', // 登陆
+    }, {
+        ruleId: 1002,
+        name: 'article_time', //文章时常
+    },{
+        ruleId: 1003,
+        name: 'video_time', //视听学 xi 时长
+    },{
+        ruleId: 6,
+        name: 'daily', //每日答题
+    },{
+        ruleId: 5,
+        name: 'weekly', //3每周答题
+    },{
+        ruleId: 4,
+        name: 'zhuanxiang', //4专项答题
+    },]
+    let scores = {}
+    dayScoreDtos.forEach(dot => {
+        rule_list.forEach(rule =>{
+            if (dot.ruleId === rule.ruleId) {
+                scores={
+                    ...scores,
+                    [rule.name]: dot.currentScore
+                }
+            }
+        })
+    });
+    scores.today = today
+    // 阅读文章，视听学 xi ，登录，文章时长，视听学 xi 时长，每日答题，每周答题，专项答题
+    return {
+        userId,
+        total,
+        scores
+    }
 }
 
 
